b65e2b40c6ab36c6b954c5097cd6775aa6ed78c5
kdbeer <kridsadacpe@gmmail.com>
2019-07-03 15:46:36 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Header />
+    <div class="wrapper">
+      <!-- Sidebar -->
+      <nav id="sidebar">
+        <Sidebar />
+      </nav>
+
+      <!-- Page Content -->
+      <div id="content">
+        <!-- We'll fill this with dummy content -->
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import Header from "./components/header.components";
+import Sidebar from "./components/sidebar.component";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Header,
+    Sidebar
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import url("./styles/style.scss");
 </style>
